818a6df3ba5ce57497ff030d7dcf2562c567f86b
edwenloke <265223864+edwenloke@users.noreply.github.com>
2026-04-07 17:25:59 +0800

    solved 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    if (word.trim() === '')
+        return '';
+    return word.split('').reverse('').join('');
 };
+
+console.log(reverseString('abc 123'));
+
 
 // Do not edit below this line
 module.exports = reverseString;
