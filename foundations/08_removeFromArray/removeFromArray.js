ce3ee865526d680cc2dbcb231829e0cd3ce07b3a
edwenloke <265223864+edwenloke@users.noreply.github.com>
2026-04-08 17:13:21 +0800

    solved 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    array.forEach(item => {
+        if (!args.includes(item)) {
+            newArray.push(item); 
+        }  
+    });
+    return newArray;
 };
+console.log(removeFromArray([1, 2, 3], 2, 3));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
