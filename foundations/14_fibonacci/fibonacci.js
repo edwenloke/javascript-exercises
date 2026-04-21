bcdbdb659ad708aa5cee3629cd70770de864da68
edwenloke <265223864+edwenloke@users.noreply.github.com>
2026-04-21 17:04:21 +0800

    solved 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (!Number.isInteger(Number(n))) {
+        return;
+    } else {
+        let count = Number(n);
+        n = count;
+    }
+    
+    if (n < 0) return 'OOPS';
+    if (n <= 1) return n;
+
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
