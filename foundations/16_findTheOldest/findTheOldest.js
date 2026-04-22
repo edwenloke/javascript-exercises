777bd6abb0e1d279327bce214661b3924ef57c9b
edwenloke <265223864+edwenloke@users.noreply.github.com>
2026-04-22 12:11:36 +0800

    solved 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function findAge(array) {
+        return array.reduce((obj, person) => {   
+        const yearOfDeath = person.yearOfDeath || new Date().getFullYear();
+        const age = yearOfDeath - person.yearOfBirth;
+   
+        obj[person.name] = age;
 
+        return obj;
+    }, {});
+};
+
+const findTheOldest = function(array) {
+    const ages = findAge(array);
+    
+    const oldestPerson = Object.entries(ages).reduce((oldest, current) => {
+        return current[1] > oldest[1] ? current : oldest
+    });
+    
+    return array.find(person => person.name === oldestPerson[0]);
 };
 
 // Do not edit below this line
