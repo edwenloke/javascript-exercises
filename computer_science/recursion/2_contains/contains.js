84b8464fa6e25df31d76d2b9951a295d8efa6825
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-07-28 02:28:37 +0800

    contains exercise

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,21 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return true;
+    }
+
+    if (Number.isNaN(value) && Number.isNaN(obj[key])) {
+      return true;
+    }
+
+    if (obj[key] !== null && typeof obj[key] === "object") {
+      const result = contains(obj[key], value);
+      if (result) return true;
+    }
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
