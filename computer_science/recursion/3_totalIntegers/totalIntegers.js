9f8806384899fc0fef8e41d0459c17dcab5fd94e
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-07-28 17:10:02 +0800

    totalIntegers exercise

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,25 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj) {
+  const isObject = (value) => typeof value === "object" && value !== null;
+
+  if (!isObject(obj)) return;
+
+  let count = 0;
+
+  const items = Object.values(obj);
+
+  for (const item of items) {
+    if (Number.isInteger(item)) {
+      count++;
+    } else if (isObject(item)) {
+      count += totalIntegers(item);
+    }
+  }
+
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
+
+// totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // returns 7
+// totalIntegers({ a: 1, b: { a: [5, 10], b: 11 } }); // returns 4
