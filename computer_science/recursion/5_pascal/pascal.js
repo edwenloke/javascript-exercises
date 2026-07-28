3bbbf97eee0fe689462b5fc48007e744e7d9936b
Edwen Loke Jian Wen <265223864+edwenloke@users.noreply.github.com>
2026-07-29 02:25:54 +0800

    pascal exercise

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,23 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) return [1];
+
+  const smaller = n - 1;
+  const array = pascal(smaller);
+
+  array.unshift(0);
+  array.push(0);
+
+  const result = array.map((element, index) => {
+    if (index === array.length - 1) {
+      return;
+    }
+
+    return (element = element + array[index + 1]);
+  });
+
+  result.splice(-1, 1);
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
